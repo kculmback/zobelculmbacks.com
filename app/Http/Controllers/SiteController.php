53d09039ff3a33eff2089b0d248301a7f5662e84
Kasey Culmback <kasey@kaseys-mbp.lan>
2019-03-17 12:30:41 -0700

    php meta data

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -13,7 +13,9 @@ class SiteController extends Controller
 
     public function main(Request $request)
     {
-        dd($request->route()->getName());
-        return view('main');
+        $name = $request->route()->getName() ?? 'default';
+        $meta_title = config("metadata.title.{$name}");
+        $meta_description = config("metadata.description.{$name}");
+        return view('main')->with(compact('meta_title', 'meta_description'));
     }
 }
