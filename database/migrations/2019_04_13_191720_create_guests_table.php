c2168d1e8510ae4da82d88cdad16879e9f0deda0
Kasey Culmback <kasey@Kaseys-MacBook-Pro.local>
2019-04-13 12:24:38 -0700

    scaffold backend invite system

---
+++ database/migrations/2019_04_13_191720_create_guests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('invite_id');
+            $table->string('name');
+            $table->boolean('rsvp')->nullable();
+            $table->timestamps();
+
+            $table->foreign('invite_id')->references('id')->on('invites')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}
