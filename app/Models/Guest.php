e4fabffa98238afcdd4ce56854e1af1cfc2cb132
Kasey Culmback <kasey@Kaseys-MacBook-Pro.local>
2019-04-13 21:44:17 -0700

    integrate search

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Guest extends Model
 {
+    use Searchable;
+
     /**
      * Hide specific attributes when serialized
      *
@@ -24,6 +27,18 @@ class Guest extends Model
      * @var array
      */
     protected $fillable = ['name', 'rsvp'];
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        return ['name' => $array['name']];
+    }
 
     public function invite()
     {
