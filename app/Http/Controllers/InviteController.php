d242f479dfa744ad0df0372105fc9942e492b6c3
Kasey Culmback <kasey@Kaseys-MacBook-Pro.local>
2019-04-13 21:44:05 -0700

    working on invite system

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Invite;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateInviteRequest;
+use App\Models\Guest;
 
 class InviteController extends Controller
 {
@@ -18,8 +19,9 @@ class InviteController extends Controller
         $take = $request->input('take') ?? 15;
 
         return response()
-            ->json(Invite::with('guests')
-            ->simplePaginate($take));
+            ->json(
+                Invite::with('guests')->simplePaginate($take)
+            );
     }
 
     /**
@@ -32,6 +34,7 @@ class InviteController extends Controller
     {
         $invite = $this->createInvite();
         $invite = $this->createGuests($invite, $request->input('guests'));
+        $invite->load('guests');
         return $invite;
     }
 
@@ -47,7 +50,8 @@ class InviteController extends Controller
 
     private function createGuests(Invite $invite, array $guests)
     {
-        return $invite->guests()->createMany($guests);
+        $invite->guests()->createMany($guests);
+        return $invite;
     }
 
     /**
@@ -58,18 +62,9 @@ class InviteController extends Controller
      */
     public function show(Invite $invite)
     {
-        //
-    }
+        $invite->load('guests');
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Invite  $invite
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Invite $invite)
-    {
-        //
+        return response()->json($invite);
     }
 
     /**
@@ -79,9 +74,68 @@ class InviteController extends Controller
      * @param  \App\Invite  $invite
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Invite $invite)
+    public function update(CreateInviteRequest $request, Invite $invite)
     {
-        //
+        $guests = $request->input('guests');
+        $current_guests = $this->filterGuests($guests, true);
+        $new_guests = $this->filterGuests($guests, false);
+
+        if (count($current_guests)) {
+            $this->updateGuests($current_guests);
+
+            $guests_to_delete = $this->getGuestsToDelete($invite, $current_guests);
+            if (count($guests_to_delete)) {
+                $this->deleteGuests($guests_to_delete);
+            }
+        }
+
+        if (count($new_guests)) {
+            $this->createGuests($invite, $new_guests);
+        }
+
+        return Invite::with('guests')->find($invite->id);
+    }
+
+    private function filterGuests($guests, $has_id)
+    {
+        return array_filter($guests, function ($guest) use ($has_id) {
+            return $has_id === !empty($guest['id']);
+        });
+    }
+
+    private function getGuestsToDelete(Invite $invite, array $new_guests)
+    {
+        $invite->load('guests');
+
+        $to_delete = [];
+
+        foreach ($invite->guests as $guest) {
+            $new_guests_contains_guest = count(array_filter($new_guests, function ($new_guest) use ($guest) {
+                return $new_guest['id'] === $guest->id;
+            }));
+
+            if (!$new_guests_contains_guest) {
+                $to_delete[] = $guest;
+            }
+        }
+
+        return $to_delete;
+    }
+
+    private function deleteGuests(array $guests)
+    {
+        foreach ($guests as $guest) {
+            $guest->delete();
+        }
+    }
+
+    private function updateGuests(array $guests)
+    {
+        foreach ($guests as $guest) {
+            $guest_model = Guest::findOrFail($guest['id']);
+            $guest_model->fill($guest);
+            $guest_model->save();
+        }
     }
 
     /**
@@ -92,6 +146,7 @@ class InviteController extends Controller
      */
     public function destroy(Invite $invite)
     {
-        //
+        $invite->delete();
+        return response()->json(['message' => 'Success']);
     }
 }
